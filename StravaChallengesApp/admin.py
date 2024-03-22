d0202565add48f6890153d1c92a75819a41a3c3d
micho-k <58048385+micho-k@users.noreply.github.com>
2024-03-22 19:57:54 +0100

    Initial commit

---
+++ StravaChallengesApp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Athlete, Activity
+
+
+class AtheteAdmin(admin.ModelAdmin):
+    list_filter = ('athlete_id', 'username')
+    list_display = ('username', 'firstname', 'lastname', 'athlete_id')
+   
+class ActivityAdmin(admin.ModelAdmin):
+    list_filter = ('activity_athlete', 'activity_type', 'start_date')
+    list_display = ('activity_athlete', 'activity_type', 'start_date', 'activity_id')
+
+admin.site.register(Athlete, AtheteAdmin)
+admin.site.register(Activity, ActivityAdmin)
